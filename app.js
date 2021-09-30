edc691128cca3cf448fc69ffeb4ae9ddc4efc300
정재희 <developer_jay@jeongjaehuiui-MacBookPro.local>
2021-09-30 18:44:58 +0900

    refactoring

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ const ajax = new XMLHttpRequest(); //출력결과를 반환시켜줌
 const NEWSURL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 const content = document.createElement('div');
-ajax.open('GET',NEWSURL,false); //false: 해당 주소의 데이터를 동기적으로 처리하겠다는 옵션.
-ajax.send(); //send: date를 가져옴.
 
 
-const newsFeed = JSON.parse(ajax.response); //응답값을 객체로 바꾸기.
+function getData(url){
+    ajax.open('GET',url,false); //false: 해당 주소의 데이터를 동기적으로 처리하겠다는 옵션.
+    ajax.send(); //send: date를 가져옴.
+
+
+    return JSON.parse(ajax.response);
+}
+
+
+
+
+const newsFeed = getData(NEWSURL);//응답값을 객체로 바꾸기.
 const ul = document.createElement('ul');
 
 
@@ -16,10 +25,7 @@ window.addEventListener('hashchange',function () {
 const id = location.hash.substr(1);
 console.log(id); //해시 데이터를 넘겨줌
 
-ajax.open('GET',CONTENT_URL.replace('@id',id),false);
-ajax.send();
-
-const newsContent = JSON.parse(ajax.response);
+const newsContent = getData(CONTENT_URL.replace('@id',id));
 const title = document.createElement('h1');
 
 title.innerHTML = newsContent.title;
@@ -30,20 +36,18 @@ content.appendChild(title);
 
 
 for (let i = 0; i < newsFeed.length; i++) {
+    const div = document.createElement('div');
 
-    const li = document.createElement('li');
-    const a = document.createElement('a');
-    // <li><a>title</a></li>
-    ul.appendChild(li)
-    li.appendChild(a)
-
-
-    a.href =`#${newsFeed[i].id}`;
-    a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
     
-
-
-
+    div.innerHTML =  `
+    <li>
+    <a href='#${newsFeed[i].id}'> ${newsFeed[i].title} (${newsFeed[i].comments_count})</a> 
+    
+    </li>
+    `;
+     
+    ul.appendChild(div.children[0]);
+    // ul.appendChild(div.firstElementChild);
 }
 
 container.appendChild(ul);
